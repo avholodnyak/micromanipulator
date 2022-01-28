373f0fe11020a9cce6dbc9a7920fcd7eeef1bca6
Andrey Holodnyak <andrey.holodnyak@gmail.com>
2022-01-28 23:10:49 +0300

    Initial structure

---
+++ src/App/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Cont } from './styled';
+import { Typography } from '@mui/material';
+
+const App = () => (
+  <Cont>
+    <Typography variant="h3">
+      DIY micromanipulator app
+    </Typography>
+  </Cont>
+);
+
+export default App;
---
+++ src/App/styled.ts
@@ -0,0 +1,8 @@
+import styled from '@emotion/styled';
+
+export const Cont = styled.div`
+  padding: 40px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
