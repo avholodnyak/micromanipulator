db33d0d7bcc34a850bc3b04208187c65e2f5ba58
Andrey Holodnyak <andrey.holodnyak@gmail.com>
2022-02-05 17:30:57 +0300

    Improve the layout

--- frontend/src/components/App/styled.ts
+++ frontend/src/components/App/styled.ts
@@ -2,10 +2,10 @@ import styled from '@emotion/styled';
 
 export const Cont = styled.div`
   padding: 40px 20px;
-  width: 600px;
-  margin: auto;
+  width: 400px;
   max-width: 100vw;
   box-sizing: border-box;
+  margin: auto;
   display: flex;
   flex-direction: column;
   align-items: center;
--- frontend/src/components/AxesControls/styled.ts
+++ frontend/src/components/AxesControls/styled.ts
@@ -3,9 +3,6 @@ import styled from '@emotion/styled';
 export const Cont = styled.div`
   display: flex;
   justify-content: space-between;
-  width: 400px;
-  max-width: 100%;
-  margin: 0 auto;
 `;
 
 export const ZCont = styled.div`
